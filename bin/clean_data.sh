d3e4a5bbabd293135f2f57da893bf3fd0e0aaf8c
ZD <zsvoboda@gmail.com>
2022-07-07 11:04:17 +0200

    Clean doesnt delete notebooks

--- bin/clean_data.sh
+++ bin/clean_data.sh
@@ -7,7 +7,7 @@ cd ${NGODS_HOME}/data/mariadb && rm -rf * .*
 cd ${NGODS_HOME}/data/minio && rm -rf * .* 
 cd ${NGODS_HOME}/data/trino && rm -rf * .* 
 cd ${NGODS_HOME}/data/postgres && rm -rf * .* 
-cd ${NGODS_HOME}/data/spark && rm -rf * .* 
+# cd ${NGODS_HOME}/data/spark && rm -rf * .* 
 cd ${NGODS_HOME}/data/elasticsearch && rm -rf * .* 
 cd ${NGODS_HOME}/data/zookeeper && rm -rf * .* 
 cd ${NGODS_HOME}/data/zookeeper && rm -rf *.html 
