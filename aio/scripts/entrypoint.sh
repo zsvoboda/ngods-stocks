9bd62b789326bed59164983805dda824f60a9b9e
Zdenek Svoboda <zsvoboda@gmail.com>
2022-07-10 22:12:09 +0200

    Minio warehouse bucket creation fix

--- aio/scripts/entrypoint.sh
+++ aio/scripts/entrypoint.sh
@@ -11,12 +11,13 @@ export HADOOP_CLASSPATH=${HADOOP_HOME}/share/hadoop/tools/lib/aws-java-sdk-bundl
 #export MINIO_ROOT_USER=minio
 #export MINIO_ROOT_PASSWORD=minio123
 
+# minio server
+${MINIO_HOME}/bin/minio server ${MINIO_DATA_ROOT} --console-address ":9001" &
+
 # pgsql
 sudo -u postgres /usr/lib/postgresql/13/bin/postgres -D /var/lib/postgresql/13/main -c config_file=/etc/postgresql/13/main/postgresql.conf &
 
-
-# minio
-${MINIO_HOME}/bin/minio server ${MINIO_DATA_ROOT} --console-address ":9001" &
+# minio ensure warehouse bucket
 until (${MINIO_HOME}/bin/mc config host add minio http://minio:9000 minio minio123) do echo '...waiting...' && sleep 1; done;
 ${MINIO_HOME}/bin/mc mb minio/warehouse
 ${MINIO_HOME}/bin/mc policy set public minio/warehouse
