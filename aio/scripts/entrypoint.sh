967d5e166f0618969b21fcc51c32270341022424
Zdenek Svoboda <zsvoboda@zdm13.lan>
2022-07-19 14:27:20 +0200

    Trino 390 in separate container

--- aio/scripts/entrypoint.sh
+++ aio/scripts/entrypoint.sh
@@ -3,13 +3,11 @@
 export JAVA_HOME="$(jrunscript -e 'java.lang.System.out.println(java.lang.System.getProperty("java.home"));')"
 export HADOOP_HOME=/opt/hadoop
 export HIVE_HOME=/opt/metastore
-export TRINO_HOME=/opt/trino
 export DAGSTER_HOME=/opt/dagster
-
 export METASTORE_HADOOP_VERSION=3.2.0
 export HADOOP_CLASSPATH=${HADOOP_HOME}/share/hadoop/tools/lib/aws-java-sdk-bundle-1.11.375.jar:${HADOOP_HOME}/share/hadoop/tools/lib/hadoop-aws-${METASTORE_HADOOP_VERSION}.jar
-
 export DAGSTER_DIR=/var/lib/ngods/dagster
+
 
 # spark
 mkdir -p /tmp/spark-events
@@ -21,9 +19,6 @@ start-thriftserver.sh --hiveconf hive.server2.thrift.port 10000 --hiveconf hive.
 # metastore 
 ${HIVE_HOME}/bin/schematool -initSchema -dbType postgres
 ${HIVE_HOME}/bin/start-metastore &
-
-# trino
-${TRINO_HOME}/bin/launcher start
 
 # dagster
 cd ${DAGSTER_DIR} && dagit -h 0.0.0.0 -p 3070 &
